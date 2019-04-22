1b798bfbb21e8a207269550b74111b4cb6cf0908
Sarah Williams <srwilliams8678@gmail.com>
2019-04-22 20:12:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,9 @@ dwarfs.each do |dwarfs|
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (elements)
+
+
 end
 
 def long_planeteer_calls# code an argument here
